237896ce1581c1a371bd6ea49666ad279c394525
Rostislav ydin <facttv777@gmail.com>
2023-06-18 07:47:37 +0300

    выполнил все задание

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -47,3 +47,5 @@ console.log(getUsersNamesInAgeRange(people, "мужской")); // 32
 console.log(getUsersNamesInAgeRange(people, "женский")); // 27.4
 console.log(getUsersNamesInAgeRange([], "женский")); // 0
 console.log(getUsersNamesInAgeRange(people, "инопланетянин")); // 0
+
+// все было легко 
